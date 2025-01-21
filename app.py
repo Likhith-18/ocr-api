a00fb16cae64bf7b784754c3a0ffe6912a266c77
Likhith-18 <vparvathi222@gmail.com>
2025-01-21 19:03:15 +0530

    Update Uvicorn server host to allow external access

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ from app.main import app
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app.main:app", host="localhost", port=3000)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=3000)
